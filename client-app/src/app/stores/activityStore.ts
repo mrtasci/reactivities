2d3aeaea304efa4f4dd7fa3e0c06b12e39f3001e
Murat Taşcı <mtasci@magiclick.com>
2020-05-10 18:55:29 +0300

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -13,9 +13,18 @@ class ActivityStore {
   @observable target = '';
 
   @computed get activitiesByDate() {
-    return Array.from(this.activityRegistry.values()).sort(
+    return this.groupActivititesByDate(Array.from(this.activityRegistry.values()));
+  }
+
+  groupActivititesByDate(activities: IActivity[]){
+    const sortedActivites = activities.sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
     );
+    return Object.entries(sortedActivites.reduce((activities, activity)=> {
+      const date = activity.date.split('T')[0];
+      activities[date] =  activities[date] ? [...activities[date], activity] : [activity];
+      return activities;
+    },{} as {[key: string]: IActivity[]}));
   }
 
   @action loadActivities = async () => {
